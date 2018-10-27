3cea54bbae074ff120b60a929c714451ae5275f5
igulu <igulu1998@outlook.com>
2018-10-27 12:06:54 +0800

    update contract

--- contract/YMHC.sol
+++ contract/YMHC.sol
@@ -30,7 +30,7 @@ contract YMHC {//is inherently ERC20
 
     uint8 constant public decimals = 8;
 
-    uint256 constant public totalSupply = 21000000e8;
+    uint256 constant public totalSupply = 300000;
 
     uint256 constant private MAX_UINT256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
@@ -56,8 +56,6 @@ contract YMHC {//is inherently ERC20
     function transfer(address _to, uint256 _value) public returns (bool success) {
         /* Ensures that tokens are not sent to address "0x0" */
         require(_to != address(0));
-        /* Prevents sending tokens directly to contracts. */
-        require(isNotContract(_to));
 
         /* SafeMathMOd.sub will throw if there is not enough balance and if the transfer value is 0. */
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
